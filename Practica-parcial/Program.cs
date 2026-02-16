ef6f138f9aac05045f802f460af3efc7f65ada73
20220936-netizen <20220936@itla.edu.do>
2026-02-15 20:12:32 -0400

    texto

--- Practica-parcial/Program.cs
+++ Practica-parcial/Program.cs
@@ -1,1 +1,4 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+
+Console.WriteLine("Hola, esta es la presentacion de la practica parcial.");
+Console.WriteLine("Este grupo esta compuesto por: Aiker Suli y Diego Gonzales.");
